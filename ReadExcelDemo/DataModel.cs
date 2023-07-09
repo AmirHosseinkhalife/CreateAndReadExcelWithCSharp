037114ad86ac7cc7dd7179499df8e14ee4716a5f
Amir Khalife <95607061+AmirHosseinkhalife@users.noreply.github.com>
2023-07-09 12:28:12 +0330

    Update DataModel.cs

--- ReadExcelDemo/DataModel.cs
+++ ReadExcelDemo/DataModel.cs
@@ -1,32 +1,16 @@
-﻿namespace ReadExcelDemo
+using System.ComponentModel.DataAnnotations;
+
+namespace ReadExcelDemo
 {
     public class DataModel
     {
-        public string? AccountNumber { get; set; }
-        public string? IBAN { get; set; }
-        public string? NationalCode { get; set; }
+        [Display(Name ="نام")]
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
-        public string? DocumentDescription { get; set; }
-
-        public string? Debtor { get; set; }
-
-        public string? Creditor { get; set; }
-
-        public string? AccountBalance { get; set; }
-
-        public string? DocumentNumber { get; set; }
-
-        public string? TerminalNumber { get; set; }
-
-        public string? TrackingCode { get; set; }
-
-        public string? BankName { get; set; }
-
-        public string? BankCode { get; set; }
-
-        public string? BranchName { get; set; }
-
-        public string? BranchCode { get; set; }
+        public string? UserName { get; set; }
+        [Display(Name ="کد ملی")]
+        public string? NationalCode { get; set; }
+        public string? PhoneNumber { get; set; }   
+     
     }
 }
